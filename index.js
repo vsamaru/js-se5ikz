1fc9748861b7f95c9bf4551e078d8e4cc965df0f
vsamaru <vsamaru@gmail.com>
2022-02-16 05:08:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const data = { a: 1, b: 2, c: 3 };
 
-const removeProp = 'b';
-
-const { [removeProp]: remove, ...rest } = data;
-
-console.log(remove); // 2
-console.log(rest); // { a: 1, c: 3 }
-
-document.getElementById("app").innerHTML += '<pre>' +JSON.stringify(rest)+'</pre></br>'//
-
-var rand = (x) => `https://p-o.surge.sh/${(x||"i")}/${String(~~(Math.random() * 809) + 1).padStart(3, '0')}.png`
+var require_html = __commonJS({
+  "src/iinl/html.js"(exports) {
 import "./html.js"
+})
+
 // src/iinl/html.js
 
 // src/iinl/bot.js<related>
