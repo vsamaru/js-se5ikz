a943a5c401e268df9c1e09fd11aa11e90ceb5ba1
vsamaru <vsamaru@gmail.com>
2022-02-16 05:07:30 +0200

    Create html.js

---
+++ html.js
@@ -0,0 +1,121 @@
+exports.html2 = (x) => `
+<!DOCTYPE HTML>
+<html xml:lang="uk" lang="uk" dir="ltr" class="device-nonmobile no-js" prefix="og: http://ogp.me/ns#">
+   <head>
+      <meta charset="utf-8">
+      <meta name="apple-mobile-web-app-status-bar-style" content="black">
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="google" content="notranslate" />
+      <meta name="description" content="Картинка Darnytsia-Amnesia1s розміщена в ImgBB">
+      <meta name="robots" content="noindex, nofollow" />
+      <title>Darnytsia-Amnesia1s - ImgBB</title>
+      <link rel="stylesheet" href="//simgbb.com/3515/ibb.css">
+      <link rel="shortcut icon" href="//simgbb.com/images/favicon.png">
+      <link rel="icon" type="image/png" href="//simgbb.com/images/favicon.png" sizes="192x192">
+      <link rel="apple-touch-icon" href="//simgbb.com/images/favicon.png" sizes="180x180">
+      <meta property="og:type" content="article" />
+      <meta property="og:url" content="https://ibb.co/pJZrrWh" />
+      <meta property="og:title" content="Darnytsia-Amnesia1s" />
+      <meta property="og:site_name" content="ImgBB" />
+      <meta property="og:description" content="Картинка Darnytsia-Amnesia1s розміщена в ImgBB" />
+      <meta property="og:image" content="https://i.ibb.co/Qrnjj8D/Darnytsia-Amnesia1s.jpg" />
+      <meta property="og:image:width" content="1280" />
+      <meta property="og:image:height" content="1280" />
+      <meta name="twitter:card" content="photo">
+      <meta name="twitter:description" content="Картинка Darnytsia-Amnesia1s розміщена в ImgBB">
+      <meta name="twitter:title" content="Darnytsia-Amnesia1s">
+      <meta name="twitter:site" content="@imgbb_com">
+      <link rel="alternate" type="application/json+oembed" href="https://ibb.co/pJZrrWh/oembed.json" title="Darnytsia-Amnesia1s - ImgBB">
+      <link rel="alternate" type="application/xml+oembed" href="https://ibb.co/pJZrrWh/oembed.xml" title="Darnytsia-Amnesia1s - ImgBB">
+      <link rel="image_src" href="https://i.ibb.co/Qrnjj8D/Darnytsia-Amnesia1s.jpg">
+   </head>
+   <body id="image" class=" no-margin-top">
+      <header id="top-bar" class="top-bar white">
+         <div class="content-width">
+            <div id="logo" class="top-bar-logo"><a href="https://imgbb.com"><img class="replace-svg" src="//simgbb.com/images/logo.png" alt="ImgBB"></a></div>
+         </div>
+      </header>
+      <div class="top-bar-placeholder"></div>
+      <div id="image-viewer" class="image-viewer full-viewer margin-bottom-10">
+         <div id="image-viewer-container" class="image-viewer-main image-viewer-container"><img src="https://i.ibb.co/syQJJ5W/Darnytsia-Amnesia1s.jpg" alt="Darnytsia-Amnesia1s" width="1280" height="1280" data-load="full"></div>
+      </div>
+      <div class="content-width">
+         <h1 class="viewer-title" data-text="image-title">Darnytsia-Amnesia1s</h1>
+         <div id="tabbed-content-group">
+            <div id="tab-about" class="tabbed-content visible">
+               <div class="c9 phablet-c1 fluid-column grid-columns">
+                  <div class="panel-description default-margin-bottom">
+                     <p class="description-text margin-bottom-5" data-text="image-description"></p>
+                     <p class="description-meta margin-bottom-5">
+                        Завантажено <span title="2022-01-17 08:55:24">6 секунд тому</span>
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
+`
+    exports.html = (x) => `
+    <html prefix="og: https://ogp.me/ns#">
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+      <meta property="og:type" content="article">
+      <meta property="og:title" content="Водохреща 2022">
+      <meta property="og:description" content="народні купання в ополонці та баня">
+      <meta property="og:image" content="https://i.ibb.co/R2v6mLM/banya-bochka-7.jpg">
+      <meta property="og:site_name" content="БАНЯ є БАНЯ">
+      <meta property="article:published_time" content="2018-01-25T07:36:53+0000">
+     
+      <meta property="telegram:channel" content="@banie">
+      <meta property="al:ios:app_name" content="Medium">
+        </head>
+      <body>
+    <article class="image-grid">
+        <a href="https://ibb.co/fSPLkH4"><img src="https://i.ibb.co/BPSFyBq/i.jpg" alt="i" border="0"></a>
+        <a href="https://ibb.co/31KDFc2"><img src="https://i.ibb.co/BLkX6Bb/i.jpg" alt="i" border="0"></a>
+        <a href="https://bulma.io/documentation/columns/basics/">Media</a><b>декоративная косметика или другие хрупкие товары нуждаются в дополнителной упаковке при отправление. Доставка таких товаров осуществляется только самолетом. Ориентировочная цена доставки от 25-40 грн за 100 грамм</b><a href="https://bulma.io/documentation/columns/basics/">wrrrr</a>
+    <related>
+    
+ <h3 class="tl_main_recent_news_header">
+   <a href="https://telegram.org/blog">Інші новини</a>
+</h3>
+<a class="dev_blog_card_link_wrap" href="https://telegram.org/blog/video-stickers-better-reactions/uk">
+   
+      <img class="dev_blog_card_image" src="https://telegram.org/file/464001234/e805/Z0pNrCJET-E.104373/a38ce74977bc53b064">
+     
+         <h4 class="dev_blog_card_title">Відеоналіпки, удосконалені реакції та інші нововведення</h4>
+ 
+  
+</a>
+<a class="dev_blog_card_link_wrap" href="https://telegram.org/blog/reactions-spoilers-translations/uk">
+   
+      <img class="dev_blog_card_image" src="https://telegram.org/file/464001488/d35b/oNi_rR0In0o.124097/c74f7d759893b78bfb"<h4 class="dev_blog_card_title">Захист повідомлень, видалення за датою, керування пристроями та інші нововведення</h4>
+         <h4 class="dev_blog_card_title">Захист повідомлень, видалення за датою, керування пристроями та інші нововведення</h4>
+         
+</a>
+<a class="dev_blog_card_link_wrap" href="https://telegram.org/blog/protected-content-delete-by-date-and-more/uk">
+   
+      <img class="dev_blog_card_image" src="https://telegram.org/file/464001572/2/u_lvhH-CjJ0.99595/a7fca60f9c9e6e193c">
+     
+         <h4 class="dev_blog_card_title">З22ахист повідомлень, видалення за датою, керування пристроями та інші нововведення</h4>
+        
+   
+</a>
+<a class="dev_blog_card_link_wrap" href="https://telegram.org/blog/shared-media-scrolling-calendar-join-requests-and-more/uk">
+  
+      <img class="dev_blog_card_image" src="https://telegram.org/file/464001930/3/yPTwxfusWZg.174275/3f847742c263cdae99">
+      
+         <h4 class="dev_blog_card_title">Календар та швидке прокручування в поширених медіа, заявки на вступ, глобальні колірні теми та інші нововведення</h4>
+   
+</a>
+</related>
+    </article>
+
+
+</body>
+  </html>
+`;
+  }
